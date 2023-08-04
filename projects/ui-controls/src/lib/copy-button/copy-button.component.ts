3b51f6e0cf12b33e504ebb23e51ec468ff62e216
codexshell <ericoderito@gmail.com>
2023-08-04 19:17:31 +0300

    chore: generate a `copy-button` component

---
+++ projects/ui-controls/src/lib/copy-button/copy-button.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-copy-button',
+  templateUrl: './copy-button.component.html',
+  styleUrls: ['./copy-button.component.css']
+})
+export class CopyButtonComponent {
+
+}
--- projects/ui-controls/src/lib/ui-controls.module.ts
+++ projects/ui-controls/src/lib/ui-controls.module.ts
@@ -4,10 +4,11 @@ import { DragDropModule } from '@angular/cdk/drag-drop';
 
 import { CardListComponent } from './card-list/card-list.component';
 import { UiControlsComponent } from './ui-controls.component';
+import { CopyButtonComponent } from './copy-button/copy-button.component';
 
 @NgModule({
-  declarations: [UiControlsComponent, CardListComponent],
+  declarations: [UiControlsComponent, CardListComponent, CopyButtonComponent],
   imports: [CommonModule, DragDropModule],
-  exports: [UiControlsComponent, CardListComponent],
+  exports: [UiControlsComponent, CardListComponent, CopyButtonComponent],
 })
 export class UiControlsModule {}
