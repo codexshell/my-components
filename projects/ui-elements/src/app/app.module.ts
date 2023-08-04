9c60dbe8e0da11434528c0a8f9b127183373716a
codexshell <ericoderito@gmail.com>
2023-08-04 20:51:47 +0300

    chore: setup the logic for a `copy-button` custom element

--- projects/ui-elements/src/app/app.module.ts
+++ projects/ui-elements/src/app/app.module.ts
@@ -1,16 +1,23 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Injector } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { createCustomElement } from '@angular/elements';
+import { UiControlsModule, CopyButtonComponent } from 'ui-controls';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, UiControlsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private injector: Injector) {}
+
+  ngDoBootstrap() {
+    const el = createCustomElement(CopyButtonComponent, {
+      injector: this.injector,
+    });
+    customElements.define('copy-button', el);
+  }
+}
